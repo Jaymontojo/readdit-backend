147949c1a3030702b72798c0f4ce49dab68986f5
Jay <julesmontojo1@gmail.com>
2022-04-15 09:45:31 +0900

    feat: add users table

---
+++ db/migrations/20220415004055_create_users_table.js
@@ -0,0 +1,22 @@
+exports.up = async function(knex) {
+    await knex.schema.createTable('users', (table) => {
+      table.increments('id');
+      table.string('name', 255).notNullable;
+      table.string('password', 255).notNullable;
+      table.timestamps(false, true);
+    });
+
+    await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp();
+  `);
+ };
+  
+ exports.down = async function(knex) {
+  return await knex.schema
+    .dropTable('users');
+ };
+ 
